b953e5a1bdb8ddd6f4a1a0ff6d560ecb8130569c
Marc Turner <marc.t1967@gmail.com>
2020-04-15 22:27:54 -0400

    add final code

--- src/Board.js
+++ src/Board.js
@@ -29,10 +29,17 @@ import './Board.css';
  **/
 
 class Board extends Component {
+    static defaultProps = {
+        nrows: 5,
+        ncols: 5,
+        chanceLightStartsOn: 0.25,
+    };
     constructor(props) {
         super(props);
-
-        // TODO: set initial state
+        this.state = {
+            hasWon: false,
+            board: this.createBoard(),
+        };
     }
 
     /** create a board nrows high/ncols wide, each cell randomly lit or unlit */
@@ -40,6 +47,13 @@ class Board extends Component {
     createBoard() {
         let board = [];
         // TODO: create array-of-arrays of true/false values
+        for (let y = 0; y < this.props.nrows; y++) {
+            let row = [];
+            for (let x = 0; x < this.props.ncols; x++) {
+                row.push(Math.random() < this.props.chanceLightStartsOn);
+            }
+            board.push(row);
+        }
         return board;
     }
 
@@ -57,22 +71,51 @@ class Board extends Component {
                 board[y][x] = !board[y][x];
             }
         }
+        // Flip initial cell
+        flipCell(y, x);
+        flipCell(y, x - 1);
+        flipCell(y, x + 1);
+        flipCell(y - 1, x);
+        flipCell(y + 1, x);
 
         // TODO: flip this cell and the cells around it
 
         // win when every cell is turned off
         // TODO: determine is the game has been won
-
+        let hasWon = board.every((row) => row.every((cell) => !cell));
         this.setState({ board, hasWon });
     }
 
     /** Render game board or winning message. */
 
     render() {
+        if (this.state.hasWon) {
+            return <h1>YOU WON!!!</h1>;
+        }
         // if the game is won, just show a winning msg & render nothing else
         // TODO
         // make table board
         // TODO
+        let tblBoard = [];
+        for (let y = 0; y < this.props.nrows; y++) {
+            let row = [];
+            for (let x = 0; x < this.props.ncols; x++) {
+                let coord = `${y}-${x}`;
+                row.push(
+                    <Cell
+                        key={coord}
+                        isLit={this.state.board[y][x]}
+                        flipCellsAroundMe={() => this.flipCellsAround(coord)}
+                    />
+                );
+            }
+            tblBoard.push(<tr key={y}>{row}</tr>);
+        }
+        return (
+            <table className='board'>
+                <tbody>{tblBoard}</tbody>
+            </table>
+        );
     }
 }
 
--- src/Cell.js
+++ src/Cell.js
@@ -1,6 +1,5 @@
-import React, {Component} from 'react'
-import "./Cell.css"
-
+import React, { Component } from 'react';
+import './Cell.css';
 
 /** A single cell on the board.
  *
@@ -16,24 +15,21 @@ import "./Cell.css"
  **/
 
 class Cell extends Component {
-  constructor(props) {
-    super(props);
-    this.handleClick = this.handleClick.bind(this);
-  }
+    constructor(props) {
+        super(props);
+        this.handleClick = this.handleClick.bind(this);
+    }
 
-  handleClick(evt) {
-    // call up to the board to flip cells around this cell
-    this.props.flipCellsAroundMe();
-  }
+    handleClick(evt) {
+        // call up to the board to flip cells around this cell
+        this.props.flipCellsAroundMe();
+    }
 
-  render() {
-    let classes = "Cell" + (this.props.isLit ? " Cell-lit" : "");
+    render() {
+        let classes = 'Cell' + (this.props.isLit ? ' Cell-lit' : '');
 
-    return (
-        <td className={classes} onClick={this.handleClick} />
-    )
-  }
+        return <td className={classes} onClick={this.handleClick} />;
+    }
 }
 
-
-export default Cell
+export default Cell;
